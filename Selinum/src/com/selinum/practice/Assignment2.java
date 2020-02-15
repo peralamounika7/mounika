6771b537c5fdd45f8dee6660135ccf17584ec746
Mounika Satya Kaveti <peralamounika7@gmail.com>
2020-02-15 12:31:38 +0530

    Files Changed Add employee

--- Selinum/src/com/selinum/practice/Assignment2.java
+++ Selinum/src/com/selinum/practice/Assignment2.java
@@ -1,5 +1,6 @@
 package com.selinum.practice;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,13 +9,14 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Assignment2 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
 		
 		 System.setProperty("webdriver.gecko.driver", "E:\\MOUNIKA\\Selinum\\geckodriver.exe");
 	      FirefoxDriver driver=new FirefoxDriver();
 	      driver.navigate().to("http://www.primusbank.qedgetech.com/");
 	      driver.manage().window().maximize();
+	      //admin login
 	      driver.findElement(By.id("txtuId")).sendKeys("Admin");
 	      driver.findElement(By.name("txtPword")).sendKeys("Admin");
 	      driver.findElement(By.id("login")).click();
@@ -30,8 +32,29 @@ public class Assignment2 {
 	    city.selectByIndex(1);
 	    driver.findElement(By.id("btn_insert")).click();
 	    
+	    Thread.sleep(3000);
+	    Alert a1=driver.switchTo().alert();
+	    String msg=a1.getText();
 	    
-	    	      
-	}
-
+		a1.accept();
+	    //validation
+	    if(msg.contains("created sucessfully"))
+	    {
+	    	System.out.println("pass");
+	    }else
+	    	if(msg.contains("plese fill in"))
+	    	{
+	    		System.out.println("warning");
+	    	}else
+	    		if(msg.contains("alresdy exist"))
+	    		{
+	    			System.out.println("fail");
+	    		}
+	    driver.findElement(By.xpath("/html/body/form/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr/td[1]/a/img")).click();
+	    Thread.sleep(20000);
+	    driver.findElement(By.xpath("/html/body/form/div/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr/td[3]/a/img")).click();
+	    driver.close();
+	    }
+	    		
 }
+
